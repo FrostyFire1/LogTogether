fddf6498d6435eac012c7dbf8fa103ca7fc1b5c4
frostyfire1 <kamilbaron02@gmail.com>
2025-06-02 17:49:12 +0200

    Star import!!!!

--- src/main/java/com/frostyfire1/logtogether/mixins/Mixin.java
+++ src/main/java/com/frostyfire1/logtogether/mixins/Mixin.java
@@ -1,6 +1,10 @@
 package com.frostyfire1.logtogether.mixins;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 import java.util.function.Supplier;
 
 import org.apache.logging.log4j.LogManager;
